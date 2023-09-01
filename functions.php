81677a4239735e73f2b173f0593cb899592b67ce
Saqib-wpbrigade <87003565+saqib-wpbrigade@users.noreply.github.com>
2023-09-01 18:46:10 +0500

    files added for base theme

---
+++ functions.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloBrigade
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'ADIDAS_VERSION', '1.0.0' );
+
+// define( '_URI', get_template_directory_uri() );
+
+// define( 'HELLO_BRIGADE_DIR', get_template_directory() );
+// define( 'HELLO_BRIGADE_INC_DIR', HELLO_BRIGADE_DIR . '/includes/' );
+
+
+// // General functions
+// require_once HELLO_BRIGADE_INC_DIR . '/general-functions.php';
+
+// // Theme setup and script/style enqueue
+// require_once HELLO_BRIGADE_INC_DIR . '/theme.php';
+
+// // WP actions/filters
+// require_once HELLO_BRIGADE_INC_DIR . '/wp-hooks.php';
+
+// // Functions/Hooks related to ACF
+// require_once HELLO_BRIGADE_INC_DIR . '/acf.php';
+
+// // AJAX functions
+// require_once HELLO_BRIGADE_INC_DIR . '/ajax.php';
+
+// // Gravity Forms Hooks
+// require_once HELLO_BRIGADE_INC_DIR . '/gform.php';
+
+// // Contains all the shortcodes
+// require_once HELLO_BRIGADE_INC_DIR . '/shortcodes.php';
+
+// // Admin functions
+// if ( is_admin() ) {
+// 	require_once HELLO_BRIGADE_INC_DIR . '/admin-functions.php';
+// }
+
+
+// if ( ! isset( $content_width ) ) {
+// 	$content_width = 800; // Pixels.
+// }
